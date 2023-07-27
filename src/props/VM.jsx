9dd6ece7d2752c145d279f15f1fec1e8ed6cbc3f
Rachit Bharadwaj <mail.rachit.bharadwaj@gmail.com>
2023-07-27 18:58:03 +0530

    improved vision-mission

--- src/props/VM.jsx
+++ src/props/VM.jsx
@@ -1,6 +1,6 @@
 const VM = (props) => {
   return (
-    <div className="flex gap-16 items-center px-24">
+    <div className="flex gap-16 items-center lg:px-24">
       <img src={props.symbol} alt="symbol" className="w-36" />
       <div className="space-y-5">
         <p className="text-4xl text-primary font-bold">{props.title}</p>
